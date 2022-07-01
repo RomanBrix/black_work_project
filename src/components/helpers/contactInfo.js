55eaa62607dd4623aa0a6d1a28b9af259c1b9ad1
RomanBrix <stetcukroman@gmail.com>
2022-07-01 10:43:12 +0300

    100% v1.0.0

--- src/components/helpers/contactInfo.js
+++ src/components/helpers/contactInfo.js
@@ -1,11 +1,11 @@
 const phone = '+380 (73) 403 89 78';
 const phoneUri = 'tel:+380734038978';
-const mail = 'gmail@gmail.com';
+// const mail = 'gmail@gmail.com';
 const mailUri = 'mailto:gmail@gmail.com';
 
-const instaUri = 'https://www.instagram.com/';
+const instaUri = 'https://www.instagram.com/balcony_kyiv_vasilkov/';
 
-const telegramUri = 'https://t.me/Roman_brx';
+const telegramUri = 'https://t.me/black_work_manager';
 
 
 
